8b835f96902bda353ffa46433ccfb70c73d324eb
Nadia <nadiasaleh26@gmail.com>
2014-11-13 01:21:33 -0500

    updates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request,url_for,redirect,render_template
-from utils import get_rating, get_winner
+import utils
 
 app=Flask(__name__)
 
@@ -8,31 +8,44 @@ def index():
     if request.method=="GET":
         return render_template("index.html", msg="")
     if request.method=="POST":
-        print("yo")
         m1 = request.form["movie1"]
         m2 = request.form["movie2"]
         button = request.form["b"]
         if ( (button=="Submit") and (m1!="") and (m2!="") ):
-            return redirect(url_for('q', movie1=m1, movie2=m2))
+            return redirect(url_for('q', movie1=m1.upper(), movie2=m2.upper()))
         return render_template("index.html", msg="Enter movies!")
             
 
-@app.route("/q/<movie1>&<movie2>", methods=["GET","POST"])
+@app.route("/q/<movie1>&&<movie2>", methods=["GET","POST"])
 def q(movie1=None, movie2=None):
-    movie1_ratings = get_rating(movie1)
-    movie2_ratings = get_rating(movie2)
-    if ((movie1_ratings==999)or(movie2_ratings==999)):
-        return render_template("error.html")#movie not found
-    results = get_winner(movie1_ratings, movie2_ratings, movie1, movie2)
-    return render_template("results.html", 
-                           winner=results[0], 
-                           winner_score=results[1][0],
-                           winner_cr=results[1][1],
-                           winner_ar=results[1][2],
-                           loser=results[2], 
-                           loser_score=results[3][0], 
-                           loser_cr=results[3][1], 
-                           loser_ar=results[3][2])
+    m1RT = utils.find_RT(movie1)
+    if m1RT == "not found":
+        return render_template("error.html", movie=movie1)
+    m2RT = utils.find_RT(movie2)
+    if m2RT == "not found":
+        return render_template("error.html", movie=movie2)
+
+    m1_ratings = {}
+    m2_ratings = {}
+    m1_ratings['RTC'] = utils.get_RTC(m1RT)
+    m2_ratings['RTC'] = utils.get_RTC(m2RT)
+    m1_ratings['RTA'] = utils.get_RTA(m1RT)
+    m2_ratings['RTA'] = utils.get_RTA(m2RT)
+    m1_ratings['avg'] = utils.avg_rtng(m1_ratings)
+    m2_ratings['avg'] = utils.avg_rtng(m2_ratings)
+    m1_ratings['title'] = movie1
+    m2_ratings['title'] = movie2
+    winner = utils.get_winner(m1_ratings,m2_ratings)
+    tb1 = utils.get_thumb(m1RT)
+    tb2 = utils.get_thumb(m2RT)
+    return render_template("results.html",
+                           winner = winner,
+                           movie1 = movie1,
+                           movie2 = movie2,
+                           m1_ratings = m1_ratings, 
+                           m2_ratings=m2_ratings, 
+                           tb1 = tb1, 
+                           tb2 = tb2)
 
 if __name__=="__main__":
    app.debug=True
--- utils.py
+++ utils.py
@@ -1,39 +1,39 @@
 import json, urllib2
 
-def get_rating(movie):
-    rating = 0
-    num_ratings = 0
-    critics_r = 0
-    num_cr = 0
-    audience_r = 0
-    num_ar = 0
+
+def find_RT(movie):
     url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=re8z35f3uhrea4vc46jz4wcg&q=%s"
     m = movie.replace(" ","+")
     url = url%(m)
     request = urllib2.urlopen(url)
     result = request.read()
     d = json.loads(result)
+    a = d['movies'][0]
     for r in d['movies']:
-        if 'ratings' in r.keys():
-            if ((r['ratings']['critics_score'])!=-1):
-                rating += (r['ratings']['critics_score'])
-                critics_r += (r['ratings']['critics_score'])                
-                num_ratings+=1
-                num_cr+=1
-            if ((r['ratings']['audience_score'])!=-1):
-                rating += (r['ratings']['audience_score'])
-                audience_r += (r['ratings']['audience_score'])
-                num_ratings+=1
-                num_ar+=1
-    if num_ratings!=0:
-        return (rating/num_ratings), (critics_r/num_cr), (audience_r/num_ar)
-    else:
-        return 999#movie not found
+        if r['title'].upper() == movie:
+            if a['title'] != movie:
+                a = r
+            elif r['year'] > a['year']:
+                a = r
+    if a['title'].upper() == movie:
+        return a
+    return "not found"
 
+def get_RTC(movie):
+    return movie['ratings']['critics_score']
+
+def get_RTA(movie):
+    return movie['ratings']['audience_score']
     
+def avg_rtng(m):
+    return (m['RTC'] + m['RTA']) / 2
 
-def get_winner(r1s, r2s, m1, m2):
-    if max(r1s[0], r2s[0]) == r1s[0]:
-        return m1, r1s, m2, r2s
-    else:
-        return m2, r2s, m1, r1s
+def get_thumb(movie):
+    if 'posters' in movie.keys():
+        return movie['posters']['thumbnail']
+    return ""
+
+def get_winner(m1, m2):
+    if m1['avg'] > m2['avg']:
+        return m1['title']
+    return m2['title']
